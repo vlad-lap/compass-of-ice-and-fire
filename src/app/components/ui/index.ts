975f4fda1d389cfc153091c43eb5501245421aaf
vlapshov <vlad.lapshov@me.com>
2026-09-21 13:17:04 +0300

    Refactoring

--- src/app/components/ui/index.ts
+++ src/app/components/ui/index.ts
@@ -2,5 +2,6 @@ export * from './autocomplete/autocomplete.component';
 export * from './autocomplete/autocomplete-trigger.directive';
 export * from './caption/caption.component';
 export * from './card/card.component';
+export * from './route-icons/route-icons.component';
 export * from './spinner/spinner.component';
 export * from './skeleton-loader/skeleton-loader.component';
